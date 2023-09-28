4e2b453843bece0baa76a47ddc4aee4ecc72fd8a
Joshua_Ajagbe <joshua@equally.ai>
2023-09-28 03:56:26 +0100

    check for latest version

--- packages/code-gen/codeGen.ts
+++ packages/code-gen/codeGen.ts
@@ -2,7 +2,10 @@ import packageJson from './package.json';
 import commander from 'commander';
 import envinfo from 'envinfo';
 import figlet from 'figlet';
+import https from 'https';
 import colors from 'colors';
+import { execSync } from 'child_process';
+import semver from 'semver';
 
 function isUsingYarn() {
   return (process.env.npm_config_user_agent || '').indexOf('yarn') === 0;
@@ -38,32 +41,26 @@ export function init() {
     .on('--help', () => {
       console.log(`    Only ${'<project-directory>'} is required.`);
       console.log();
-      console.log(`    A custom ${'--scripts-version'.cyan} can be one of:`);
-      console.log(`      - a specific npm version: ${'0.8.2'.green}`);
-      console.log(`      - a specific npm tag: ${'@next'.green}`);
+      console.log(`    A custom ${'--scripts-version'} can be one of:`);
+      console.log(`      - a specific npm version: ${'0.8.2'}`);
+      console.log(`      - a specific npm tag: ${'@next'}`);
       console.log(
-        `      - a custom fork published on npm: ${'code-gen-scripts'.green}`
+        `      - a custom fork published on npm: ${'code-gen-scripts'}`
       );
       console.log(
-        `      - a local path relative to the current working directory: ${
-          'file:../code-gen-scripts'.green
-        }`
+        `      - a local path relative to the current working directory: ${'file:../code-gen-scripts'}`
       );
       console.log();
-      console.log(`    A custom ${'--template'.cyan} can be one of:`);
+      console.log(`    A custom ${'--template'} can be one of:`);
       console.log(
-        `      - a custom template published on npm: ${
-          'cra-template-typescript'.green
-        }`
+        `      - a custom template published on npm: ${'code-gen-template-typescript'}`
       );
       console.log();
       console.log(
         `    If you have any problems, do not hesitate to file an issue:`
       );
       console.log(
-        `      ${
-          'https://github.com/Joepolymath/express-code-gen/issues'.green
-        }`
+        `      ${'https://github.com/Joepolymath/express-code-gen/issues'}`
       );
       console.log();
     })
@@ -106,4 +103,59 @@ export function init() {
     console.log(`Run ${`${program.name()} --help`} to see all options.`);
     process.exit(1);
   }
+
+  checkForLatestVersion()
+    .catch(() => {
+      try {
+        return execSync('npm view express-code-gen version').toString().trim();
+      } catch (e) {
+        return null;
+      }
+    })
+    .then((latest) => {
+      if (latest && semver.lt(packageJson.version, latest)) {
+        console.log();
+        console.error(
+          `You are running \`express-code-gen\` ${packageJson.version}, which is behind the latest release (${latest}).\n\n` +
+            'We recommend always using the latest version of express-code-gen if possible.'
+        );
+        console.log();
+        console.log();
+      } else {
+        const useYarn = isUsingYarn();
+        createApp(
+          projectName,
+          program.verbose,
+          program.scriptsVersion,
+          program.template,
+          useYarn,
+          program.usePnp
+        );
+      }
+    });
+}
+
+function createApp(name, verbose, version, template, useYarn, usePnp) {}
+
+function checkForLatestVersion() {
+  return new Promise((resolve, reject) => {
+    https
+      .get(
+        'https://registry.npmjs.org/-/package/express-code-gen/dist-tags',
+        (res) => {
+          if (res.statusCode === 200) {
+            let body = '';
+            res.on('data', (data) => (body += data));
+            res.on('end', () => {
+              resolve(JSON.parse(body).latest);
+            });
+          } else {
+            reject();
+          }
+        }
+      )
+      .on('error', () => {
+        reject();
+      });
+  });
 }
